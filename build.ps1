0762c52964c65085303fd6ecafd62255ea36e53b
Joao Marcos <joaomarcos@eberti.com.br>
2026-08-03 08:19:02 -0300

    Mapa da home: popup do pino agora leva para a pagina da obra (fotos e detalhes)

--- build.ps1
+++ build.ps1
@@ -1393,7 +1393,7 @@ $mi = 0
 foreach($o in $mapObras){
   [void]$mapList.Append("<a class=`"mp__it`" href=`"/obras/$($o._slug)`" data-i=`"$mi`"><img src=`"/$($o.cover)`" alt=`"$(AttrEnc $o.title)`" loading=`"lazy`" /><div class=`"tx`"><div class=`"tag`">&bull; Em andamento</div><h3>$(HtmlEnc $o.title)</h3></div></a>")
   $addr = if($o.address){ $o.address } else { $o.city }
-  [void]$mapData.Append("{t:`"$(JsonStr $o.title)`",c:`"/$($o.cover)`",a:`"$(JsonStr $addr)`",lat:$(Num $o.lat),lng:$(Num $o.lng)},")
+  [void]$mapData.Append("{t:`"$(JsonStr $o.title)`",c:`"/$($o.cover)`",a:`"$(JsonStr $addr)`",lat:$(Num $o.lat),lng:$(Num $o.lng),s:`"$($o._slug)`"},")
   $mi++
 }
 
@@ -1603,7 +1603,7 @@ $homeMain = @"
       var o=MAP[i];
       var icon=L.divIcon({className:'',html:'<div style="width:54px;height:66px"><div style="width:54px;height:54px;border-radius:13px;overflow:hidden;border:3px solid #47b6f1;box-shadow:0 8px 18px rgba(6,25,34,.4);background:#fff center/cover no-repeat;background-image:url(\''+o.c+'\')"></div><div style="position:absolute;left:50%;bottom:0;transform:translateX(-50%) rotate(45deg);width:14px;height:14px;background:#47b6f1;border-radius:3px"></div></div>',iconSize:[54,66],iconAnchor:[27,66],popupAnchor:[0,-62]});
       var m=L.marker([o.lat,o.lng],{icon:icon}).addTo(map);
-      m.bindPopup('<div style="width:220px;font-family:Open Sans,sans-serif"><img src="'+o.c+'" alt="'+o.t+'" style="width:100%;height:115px;object-fit:cover;display:block"/><div style="padding:11px 13px"><div style="font-family:Barlow Condensed,sans-serif;font-weight:800;font-size:17px;text-transform:uppercase;color:#061922;line-height:1">'+o.t+'</div><div style="margin-top:5px;font-size:12.5px;color:#5b6b75">'+o.a+'</div></div></div>',{minWidth:220});
+      m.bindPopup('<a href="/obras/'+o.s+'" style="width:220px;display:block;font-family:Open Sans,sans-serif;text-decoration:none;color:inherit;cursor:pointer"><img src="'+o.c+'" alt="'+o.t+'" style="width:100%;height:115px;object-fit:cover;display:block"/><div style="padding:11px 13px"><div style="font-family:Barlow Condensed,sans-serif;font-weight:800;font-size:17px;text-transform:uppercase;color:#061922;line-height:1">'+o.t+'</div><div style="margin-top:5px;font-size:12.5px;color:#5b6b75">'+o.a+'</div><div style="margin-top:8px;font-family:Barlow Condensed,sans-serif;font-weight:700;font-size:12px;letter-spacing:.06em;text-transform:uppercase;color:#077fbf">Ver detalhes da obra &rarr;</div></div></a>',{minWidth:220});
       mk.push(m);
     }
     var lis=document.querySelectorAll('.mp__it');
